f92b87989d409a0769461a5263fa777e9135306b
Ritu Bankey <bankeyritu@gmail.com>
2023-10-04 18:06:13 +0530

    registration

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express")
 const app = express()
 app.use(express.json())
 const userRouter = require("./api/User/user_router")
-app.use("/api/user", userRouter)
+app.use("/api/users", userRouter)
 
 app.listen(process.env.APP_PORT, () => {
     console.log("connected", process.env.APP_PORT)
