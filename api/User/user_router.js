80e8313fe26378c28eaff3053f130423db1ef273
Ritu Bankey <bankeyritu@gmail.com>
2023-10-05 11:42:28 +0530

    registration data update,delete

--- api/User/user_router.js
+++ api/User/user_router.js
@@ -1,5 +1,9 @@
-const { createUser } = require("./user_controller")
+const { createUser, getUserById, getUsers, deleteUser, updateUser } = require("./user_controller")
 const router = require("express").Router()
 
 router.post("/", createUser)
+router.get("/", getUsers)
+router.get("/:id", getUserById)
+router.patch("/", updateUser)
+router.delete("/", deleteUser)
 module.exports = router
